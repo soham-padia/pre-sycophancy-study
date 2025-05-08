fb3b019c9e640f911453dce86092ed23bb1f6ded
wlqmfl <jiseung.hong@kaist.ac.kr>
2025-05-08 08:32:56 -0400

    Minor fix

--- false-presuppositions-setting/evaluate_ToF.py
+++ false-presuppositions-setting/evaluate_ToF.py
@@ -459,17 +459,17 @@ def main(args):
             "file": os.path.join(input_dir, "prompt2.csv"), 
             "output": os.path.join(output_dir, "prompt2_results.txt"),
             "all_results": os.path.join(output_dir, "prompt2_result_all.csv")
-        },
-        {
-            "file": os.path.join(input_dir, "prompt3.csv"), 
-            "output": os.path.join(output_dir, "prompt3_results.txt"),
-            "all_results": os.path.join(output_dir, "prompt3_result_all.csv")
-        },
-        {
-            "file": os.path.join(input_dir, "prompt4.csv"), 
-            "output": os.path.join(output_dir, "prompt4_results.txt"),
-            "all_results": os.path.join(output_dir, "prompt4_result_all.csv")
         }
+        # {
+        #     "file": os.path.join(input_dir, "prompt3.csv"), 
+        #     "output": os.path.join(output_dir, "prompt3_results.txt"),
+        #     "all_results": os.path.join(output_dir, "prompt3_result_all.csv")
+        # },
+        # {
+        #     "file": os.path.join(input_dir, "prompt4.csv"), 
+        #     "output": os.path.join(output_dir, "prompt4_results.txt"),
+        #     "all_results": os.path.join(output_dir, "prompt4_result_all.csv")
+        # }
     ]
     
     all_results = {}
